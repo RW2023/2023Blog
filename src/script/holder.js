4c80414fe9797ed2048bc9537c1780f3eac88ac2
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-02-06 02:11:38 -0500

    Holy Crap the JS Works! Fallback!

--- src/script/holder.js
+++ src/script/holder.js
@@ -1,8 +1,27 @@
-// Select the media object element
-var mediaObject = document.querySelector(".media");
+const images = document.querySelectorAll("img[data-src]");
 
-// Listen for a click event on the media object
-mediaObject.addEventListener("click", function () {
-  // Toggle the class that expands the media object
-  mediaObject.classList.toggle("media-expanded");
+function loadImage(img) {
+  const src = img.getAttribute("data-src");
+  if (!src) return;
+
+  img.setAttribute("src", src);
+  img.removeAttribute("data-src");
+}
+
+const options = {
+  threshold: 0,
+  rootMargin: "0px 0px 200px 0px",
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+    const img = entry.target;
+    loadImage(img);
+    observer.unobserve(img);
+  });
+}, options);
+
+images.forEach((img) => {
+  observer.observe(img);
 });
